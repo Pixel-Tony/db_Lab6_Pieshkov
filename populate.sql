f53565ed7f058dfb3087d4d18579739b50de884e
Pixel-Tony <ant.peshkoff9@gmail.com>
2023-11-05 18:20:30 +0100

    Updated total price calculation

--- populate.sql
+++ populate.sql
@@ -4,112 +4,126 @@ INSERT INTO
 VALUES
     (1, 'Classic'),
     (2, 'Supreme'),
-    (3, 'Veggie');
+    (3, 'Chicken');
 INSERT INTO
     Ingredients (ingredient_id, ingredient_name)
 VALUES
-    (1, 'Mozzarella Cheese'),
-    (2, 'Pepperoni'),
-    (3, 'Tomato Sauce'),
-    (4, 'Coarse Sicilian Salami'),
-    (5, 'Green Olives'),
-    (6, 'Luganega Sausage'),
-    (7, 'Tomatoes'),
-    (8, 'Onions'),
-    (9, 'Garlic'),
-    (10, 'Pineapple'),
-    (11, 'Sliced Ham'),
-    (12, 'Bacon'),
-    (13, 'Chorizo Sausage'),
-    (14, 'Italian Sausage'),
-    (15, 'Smoked Gouda Cheese'),
-    (16, 'Provolone Cheese'),
-    (17, 'Romano Cheese'),
-    (18, 'Blue Cheese'),
-    (19, 'Ricotta Cheese'),
-    (20, 'Gorgonzola Piccante Cheese'),
-    (21, 'Parmigiano Reggiano Cheese'),
-    (22, 'Calabrese Salami'),
-    (23, 'Capocollo'),
-    (24, 'Red Onions');
+    (1, 'Goat Cheese'),
+    (2, 'Red Peppers'),
+    (3, 'Capocollo'),
+    (4, 'Tomatoes'),
+    (5, 'Garlic'),
+    (6, 'Mozzarella Cheese'),
+    (7, 'Oregano'),
+    (8, 'Tomato Sauce'),
+    (9, 'Anchovies'),
+    (10, 'Red Onions'),
+    (11, 'Green Olives'),
+    (12, 'Pepperoni'),
+    (13, 'Artichokes'),
+    (14, 'Asiago Cheese'),
+    (15, 'Kalamata Olives'),
+    (16, 'Spinach'),
+    (17, 'Genoa Salami'),
+    (18, 'Chipotle Sauce'),
+    (19, 'Corn'),
+    (20, 'Chicken'),
+    (21, 'Cilantro'),
+    (22, 'Jalapeno Peppers'),
+    (23, 'Alfredo Sauce'),
+    (24, 'Mushrooms');
 INSERT INTO
     Pizzas (pizza_id, pizza_name, pizza_size, pizza_price, category_id)
 VALUES
-    ('pepperoni_l', 'The Pepperoni Pizza', 'L', 15.25, 1),
-    ('sicilian_m', 'The Sicilian Pizza', 'M', 16.25, 2),
-    ('hawaiian_s', 'The Hawaiian Pizza', 'S', 10.5, 1),
-    ('big_meat_s', 'The Big Meat Pizza', 'S', 12, 1),
-    ('five_cheese_l', 'The Five Cheese Pizza', 'L', 18.5, 3),
-    ('four_cheese_l', 'The Four Cheese Pizza', 'L', 17.95, 3),
-    ('ital_supr_l', 'The Italian Supreme Pizza', 'L', 20.75, 2);
+    ('ital_cpcllo_m', 'The Italian Capocollo Pizza', 'M', 16, 1),
+    ('napolitana_m', 'The Napolitana Pizza', 'M', 16, 1),
+    ('spinach_supr_m', 'The Spinach Supreme Pizza', 'M', 16.5, 2),
+    ('peppr_salami_l', 'The Pepper Salami Pizza', 'L', 20.75, 2),
+    ('ital_cpcllo_l', 'The Italian Capocollo Pizza', 'L', 20.5, 1),
+    ('southw_ckn_l', 'The Southwest Chicken Pizza', 'L', 20.75, 3),
+    ('ckn_alfredo_s', 'The Chicken Alfredo Pizza', 'S', 12.75, 3);
 INSERT INTO
     Orders (order_id, accepted_at, total_price)
 VALUES
-    (1, '09.01.2015 11:16:21', 0),
-    (2, '23.07.2015 13:35:09', 0),
-    (3, '28.10.2015 17:09:28', 0),
-    (4, '09.11.2015 12:20:39', 0),
-    (5, '31.12.2015 19:02:33', 0);
+    (1, '16.03.2015 20:29:39', 0),
+    (2, '09.06.2015 12:07:47', 0),
+    (3, '02.07.2015 12:38:12', 0),
+    (4, '29.07.2015 16:55:41', 0),
+    (5, '04.12.2015 17:02:51', 0);
 INSERT INTO
     Orders_Pizzas (order_id, pizza_id, quantity)
 VALUES
-    (1, 'pepperoni_l', 1),
-    (1, 'sicilian_m', 1),
-    (2, 'hawaiian_s', 1),
-    (3, 'big_meat_s', 1),
-    (4, 'five_cheese_l', 1),
-    (5, 'four_cheese_l', 1),
-    (5, 'ital_supr_l', 1);
+    (1, 'ital_cpcllo_m', 1),
+    (1, 'napolitana_m', 1),
+    (1, 'spinach_supr_m', 1),
+    (2, 'peppr_salami_l', 1),
+    (3, 'ital_cpcllo_l', 1),
+    (3, 'southw_ckn_l', 1),
+    (4, 'napolitana_m', 1),
+    (5, 'ckn_alfredo_s', 1);
 INSERT INTO
     Pizzas_Ingredients (pizza_id, ingredient_id)
 VALUES
-    ('pepperoni_l', 1),
-    ('pepperoni_l', 2),
-    ('pepperoni_l', 3),
-    ('sicilian_m', 4),
-    ('sicilian_m', 5),
-    ('sicilian_m', 6),
-    ('sicilian_m', 7),
-    ('sicilian_m', 1),
-    ('sicilian_m', 8),
-    ('sicilian_m', 9),
-    ('sicilian_m', 3),
-    ('hawaiian_s', 1),
-    ('hawaiian_s', 10),
-    ('hawaiian_s', 11),
-    ('hawaiian_s', 3),
-    ('big_meat_s', 12),
-    ('big_meat_s', 2),
-    ('big_meat_s', 1),
-    ('big_meat_s', 13),
-    ('big_meat_s', 14),
-    ('big_meat_s', 3),
-    ('five_cheese_l', 15),
-    ('five_cheese_l', 16),
-    ('five_cheese_l', 17),
-    ('five_cheese_l', 1),
-    ('five_cheese_l', 18),
-    ('five_cheese_l', 9),
-    ('five_cheese_l', 3),
-    ('four_cheese_l', 19),
-    ('four_cheese_l', 1),
-    ('four_cheese_l', 20),
-    ('four_cheese_l', 21),
-    ('four_cheese_l', 9),
-    ('four_cheese_l', 3),
-    ('ital_supr_l', 5),
-    ('ital_supr_l', 22),
-    ('ital_supr_l', 23),
-    ('ital_supr_l', 7),
-    ('ital_supr_l', 24),
-    ('ital_supr_l', 1),
-    ('ital_supr_l', 9),
-    ('ital_supr_l', 3);
+    ('ital_cpcllo_m', 1),
+    ('ital_cpcllo_m', 2),
+    ('ital_cpcllo_m', 3),
+    ('ital_cpcllo_m', 4),
+    ('ital_cpcllo_m', 5),
+    ('ital_cpcllo_m', 6),
+    ('ital_cpcllo_m', 7),
+    ('ital_cpcllo_m', 8),
+    ('napolitana_m', 9),
+    ('napolitana_m', 10),
+    ('napolitana_m', 4),
+    ('napolitana_m', 5),
+    ('napolitana_m', 6),
+    ('napolitana_m', 11),
+    ('napolitana_m', 8),
+    ('spinach_supr_m', 12),
+    ('spinach_supr_m', 10),
+    ('spinach_supr_m', 13),
+    ('spinach_supr_m', 6),
+    ('spinach_supr_m', 5),
+    ('spinach_supr_m', 14),
+    ('spinach_supr_m', 15),
+    ('spinach_supr_m', 16),
+    ('spinach_supr_m', 4),
+    ('spinach_supr_m', 8),
+    ('peppr_salami_l', 12),
+    ('peppr_salami_l', 17),
+    ('peppr_salami_l', 3),
+    ('peppr_salami_l', 14),
+    ('peppr_salami_l', 5),
+    ('peppr_salami_l', 6),
+    ('peppr_salami_l', 4),
+    ('peppr_salami_l', 8),
+    ('ital_cpcllo_l', 1),
+    ('ital_cpcllo_l', 2),
+    ('ital_cpcllo_l', 3),
+    ('ital_cpcllo_l', 4),
+    ('ital_cpcllo_l', 5),
+    ('ital_cpcllo_l', 6),
+    ('ital_cpcllo_l', 7),
+    ('ital_cpcllo_l', 8),
+    ('southw_ckn_l', 18),
+    ('southw_ckn_l', 10),
+    ('southw_ckn_l', 2),
+    ('southw_ckn_l', 19),
+    ('southw_ckn_l', 6),
+    ('southw_ckn_l', 20),
+    ('southw_ckn_l', 21),
+    ('southw_ckn_l', 22),
+    ('southw_ckn_l', 4),
+    ('ckn_alfredo_s', 10),
+    ('ckn_alfredo_s', 2),
+    ('ckn_alfredo_s', 23),
+    ('ckn_alfredo_s', 14),
+    ('ckn_alfredo_s', 24),
+    ('ckn_alfredo_s', 6),
+    ('ckn_alfredo_s', 20);
 
 UPDATE orders
 SET
-    order_id = sub.order_id,
-    accepted_at = sub.accepted_at,
     total_price = sub.total_price
 FROM
     (
@@ -122,8 +136,6 @@ FROM
             NATURAL JOIN orders_pizzas
             NATURAL JOIN pizzas
         GROUP BY
-            order_id
-        ORDER BY
             order_id
     ) AS sub
 WHERE
